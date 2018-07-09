0baa5ee81ff2e494e81769a52c0b9ce25018548d
André Spadetto Leão <andre.sp.leao@gmail.com>
2018-07-09 13:03:51 -0300

    Get tag UUID number.

--- app/src/main/java/qualityautomacao/com/nfceexample/dto/TagDto.kt
+++ app/src/main/java/qualityautomacao/com/nfceexample/dto/TagDto.kt
@@ -7,12 +7,8 @@ data class TagDto(val id: ByteArray, val techList: Array<out String>?) {
     }
 
     private fun convertTagIdToString(tagId: ByteArray): String {
-        val hexArray = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
         return tagId.map {
-            val incoming = it.toInt() and 0xff
-            val firstByte = incoming shr 4 and 0x0f
-            val secondByte = incoming and 0x0f
-            hexArray[firstByte] + hexArray[secondByte]
+            (it.toInt() and 0xff).toString(16)
         }.joinToString("")
     }
 }
